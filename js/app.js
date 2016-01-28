e12397e5c769567d2b4fbecdedd9f9e6a69b58ff
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-01-28 17:20:10 +0300

    GET

--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
 function loadPhones() {
   var xhr = new XMLHttpRequest();
-
-  xhr.open('GET', 'files/test.json', false);
+  xhr.open('GET', '/files/test.json', true);
   xhr.send();
 
-  if (xhr.status != 200) {
-    // обработать ошибку
-    alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
-  } else {
-    // вывести результат
-    alert(xhr.responseText);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4) return;
+    button.parentNode.removeChild(button);
+    if (xhr.status != 200) {
+      // обработать ошибку
+      alert( xhr.status + ': ' + xhr.statusText );
+    } else {
+      try {
+        var phones = JSON.parse(xhr.responseText);
+      } catch (e) {
+        alert( "Некорректный ответ " + e.message );
+      }
+      showPhones(phones);
+    }
   }
+
+  button.innerHTML = 'Загружаю...';
+  button.disabled = true;
 }
+
+function showPhones(phones) {
+
+    phones.forEach(function(phone) {
+      var li = list.appendChild(document.createElement('li'));
+      li.innerHTML = phone.name;
+    });
+
+  }
