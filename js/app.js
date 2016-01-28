be82ba23798f97610a10e8d09015e2e98dbbef5d
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-01-28 22:26:14 +0300

    last commit http get

--- js/app.js
+++ js/app.js
@@ -1,29 +1,36 @@
-function loadPhones() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/files/test.json', true);
-  xhr.send();
 
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState != 4) return;
-    button.parentNode.removeChild(button);
-    if (xhr.status != 200) {
-      // обработать ошибку
-      alert( xhr.status + ': ' + xhr.statusText );
-    } else {
-      try {
-        var phones = JSON.parse(xhr.responseText);
-      } catch (e) {
-        alert( "Некорректный ответ " + e.message );
+  function loadPhones() {
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'files/phones.json', true);
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState != 4) return;
+
+      button.parentNode.removeChild(button);
+
+      if (xhr.status != 200) {
+        // обработать ошибку
+        alert(xhr.status + ': ' + xhr.statusText);
+      } else {
+        try {
+          var phones = JSON.parse(xhr.responseText);
+        } catch (e) {
+          alert("Некорректный ответ " + e.message);
+        }
+        showPhones(phones);
       }
-      showPhones(phones);
+
     }
+
+    xhr.send();
+
+    button.innerHTML = 'Загружаю...';
+    button.disabled = true;
   }
 
-  button.innerHTML = 'Загружаю...';
-  button.disabled = true;
-}
-
-function showPhones(phones) {
+  function showPhones(phones) {
 
     phones.forEach(function(phone) {
       var li = list.appendChild(document.createElement('li'));
